5eca2d25263259be4037faa0358b5c81b30150d7
Gabe Klein <gabe@termtm.com>
2021-08-26 14:12:53 -0600

    explicitly require codemirror modules

--- src/codemirror/fancy.ts
+++ src/codemirror/fancy.ts
@@ -1,21 +1,12 @@
 import { autocompletion } from '@codemirror/autocomplete';
-import { foldGutter } from '@codemirror/fold';
 import { highlightActiveLineGutter, lineNumbers } from '@codemirror/gutter';
-import { bracketMatching } from '@codemirror/matchbrackets';
-import { highlightSelectionMatches } from '@codemirror/search';
-import { EditorState } from '@codemirror/state';
-import { highlightActiveLine, highlightSpecialChars } from '@codemirror/view';
+import { highlightActiveLine } from '@codemirror/view';
 
-export const fancy = [
-  EditorState.allowMultipleSelections.of(true),
-  highlightSelectionMatches(),
-  bracketMatching(),
-  highlightSpecialChars(),
+export const autocomplete = [
   autocompletion()
 ]
 
 export const lines = [
-  foldGutter(),
   lineNumbers(),
   highlightActiveLine(),
   highlightActiveLineGutter()
