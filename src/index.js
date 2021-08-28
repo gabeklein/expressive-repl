f4c8cc0542962d6173166864958db0b2b9dd2b38
Gabe Klein <gabe@termtm.com>
2021-08-28 02:12:13 -0600

    optimize layout

--- src/index.js
+++ src/index.js
@@ -4,40 +4,27 @@ import { Provider } from '@expressive/mvc';
 import { forwardRef, useMemo, useState } from 'react';
 
 import REPL from './codemirror';
+import { Col, Row } from './components'
 import { EditInput, LiveResult, MockOutput } from './components/Editor'
 
-App: {
+const App = () => do {
+  const controller = REPL.use();
+
   height: "100vh";
   display: flex;
   flexDirection: column;
   boxSizing: border-box;
   fontFamily: "Lato";
   padding: 5, 10;
-}
-
-column: {
-  display: flex;
-  flexDirection: column;
-  flex: 1;
-}
-
-row: {
-  display: flex;
-  flexDirection: row;
-  flex: 1;
-}
-
-const App = () => do {
-  const controller = REPL.use();
 
   <Provider of={controller}>
-    <row>
+    <Row>
       <EditInput />
-      <column>
+      <Col>
         <MockOutput />
         <LiveResult />
-      </column>
-    </row>
+      </Col>
+    </Row>
   </Provider>
 }
 
