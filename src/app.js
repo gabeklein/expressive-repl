74e0b6423a7d93a85e961aa5df0213eebad444e4
Gabe Klein <gabe@termtm.com>
2021-08-30 18:45:53 -0600

    separate app entry point

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import { Provider } from '@expressive/mvc';
+import { forwardRef, useMemo, useState } from 'react';
+
+import Control from './codemirror';
+import { Column, Row } from './resizable';
+import { EditInput, LiveResult, MockOutput } from './components/Editor';
+
+export default () => do {
+  const { get, layout } = Control.use();
+
+  <Provider of={get}>
+    <Row>
+      <EditInput />
+      <Interface layout={layout} />
+    </Row>
+  </Provider>
+}
+
+const Interface = ({ layout }) => do {
+  if(layout == "compact")
+    <Column>
+      <MockOutput />
+      <LiveResult />
+    </Column>
+  else if(layout == "fill")
+    <this>
+      <MockOutput />
+      <LiveResult />
+    </this>
+  else if(layout == "view")
+    <LiveResult />
+  else if(layout == "code")
+    <EditInput />
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,12 @@
 import './styles.css';
 
-import { Provider } from '@expressive/mvc';
-import { forwardRef, useMemo, useState } from 'react';
+import App from "./app";
 
-import Control from './codemirror';
-import { EditInput, LiveResult, MockOutput } from './components/Editor';
-import { Column, Row } from './resizable';
-
-const App = () => do {
-  const { get, layout } = Control.use();
-
+export default () => do {
   height: "100vh";
   display: grid;
   boxSizing: border-box;
   padding: 10;
 
-  <Provider of={get}>
-    <Row>
-      <EditInput />
-      <Interface layout={layout} />
-    </Row>
-  </Provider>
+  <App />
 }
-
-const Interface = ({ layout }) => do {
-  if(layout == "compact")
-    <Column>
-      <MockOutput />
-      <LiveResult />
-    </Column>
-  else if(layout == "fill")
-    <this>
-      <MockOutput />
-      <LiveResult />
-    </this>
-  else if(layout == "view")
-    <LiveResult />
-  else if(layout == "code")
-    <EditInput />
-}
-
-export default App;
