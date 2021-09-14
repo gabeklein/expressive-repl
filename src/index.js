995d22ce92ed8bb77141e47457c5446c8a514876
Gabe Klein <gabe@termtm.com>
2021-09-14 00:09:03 -0600

    bootstrap webpack 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './styles.css';
 
+import React from "react";
+import ReactDOM from "react-dom";
+
 import App from "./app";
 
-export default App;
+window.addEventListener("load", () => {
+  const container = document.getElementById("react-root");
+  const element = React.createElement(App);
+  ReactDOM.render(element, container);
+});
