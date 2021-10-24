c98c3594e4278cfce01c0ca1bebf595c7c91d7db
Gabe Klein <gabe@termtm.com>
2021-10-24 14:41:03 -0400

    consolidate document control

--- src/editor/control.ts
+++ src/editor/control.ts
@@ -2,6 +2,7 @@ import Model, { from, parent, ref, use } from '@expressive/mvc';
 
 import { build, transform } from '../transform';
 import { evaluate } from '../transform/evaluate';
+import { Document } from './document';
 
 enum Layout {
   Compact = "compact",
@@ -10,15 +11,8 @@ enum Layout {
   PreviewOnly = "view"
 }
 
-const DEFAULT_CODE =
-`export const Hi = () => do {
-  <this>Hello World!</this>
-}`
-
 export class REPL extends Model {
-  document = use(Document, doc => {
-    doc.source = localStorage.getItem("REPL:file") || DEFAULT_CODE;
-  });
+  document = use(Document);
 
   Render = from(this.generatePreview);
 
@@ -35,58 +29,10 @@ export class REPL extends Model {
   }
 
   generatePreview(){
-    const { output_js, error } = this.document;
+    const module = this.document.output;
+    const exported = Object.values(module)[0];
 
-    try {
-      if(!output_js)
-        return;
-
-      const module = evaluate(output_js);
-      const output = Object.values(module)[0];
-
-      if(typeof output === "function")
-        return output as React.FC<{}>;
-    }
-    catch(err){
-      console.error(err);
-      error("Error while evaluating module.");
-    }
-  }
-}
-
-class Document extends Model {
-  parent = parent(REPL);
-  source = "";
-
-  output_jsx = from(this.transform);
-  output_js = from(this.build);
-
-  error = ref<string>();
-  stale = false;
-
-  transform(){
-    const { source, parent } = this;
-
-    try {
-      return transform(source, parent.options);
-    }
-    catch(error){
-      console.error(error);
-      this.error("Error while compiling module.");
-    }
-    finally {
-      this.stale = false;
-      localStorage.setItem("REPL:file", source);
-    }
-  }
-
-  build(){
-    try {
-      return build(this.source);
-    }
-    catch(error){
-      this.error("Error while building preview.");
-      console.error(error)
-    }
+    if(exported)
+      return exported as React.FC<{}>;
   }
 }
---
+++ src/editor/document.ts
@@ -0,0 +1,67 @@
+import Model, { from, parent, ref } from '@expressive/mvc';
+
+import { build, transform } from '../transform';
+import { evaluate } from '../transform/evaluate';
+import { REPL } from './control';
+
+const DEFAULT_CODE =
+`export const Hi = () => do {
+  <this>Hello World!</this>
+}`
+
+export class Document extends Model {
+  parent = parent(REPL);
+  source = "";
+
+  output_jsx = from(this.transform);
+  output_js = from(this.build);
+  output = from(this.eval);
+
+  error = ref<string>();
+  stale = false;
+
+  didCreate(){
+    this.source =
+      localStorage.getItem("REPL:file") || DEFAULT_CODE;
+  }
+
+  transform(){
+    const { source, parent } = this;
+
+    try {
+      return transform(source, parent.options);
+    }
+    catch(error){
+      console.error(error);
+      this.error("Error while compiling module.");
+    }
+    finally {
+      this.stale = false;
+      localStorage.setItem("REPL:file", source);
+    }
+  }
+
+  build(){
+    try {
+      return build(this.source);
+    }
+    catch(error){
+      this.error("Error while building preview.");
+      console.error(error)
+    }
+  }
+
+  eval(){
+    if(!this.output_js)
+      return {};
+
+    try {
+      return evaluate(this.output_js);
+    }
+    catch(error){
+      this.error("Error while building preview.");
+      console.error(error);
+      return {};
+    }
+  }
+}
