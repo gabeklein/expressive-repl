85a729c5282d0107026eca4ce164279d0b3f05db
Gabe Klein <gabe@termtm.com>
2023-06-04 15:11:31 -0400

    separate build in OutputView

--- src/editor/OutputView.ts
+++ src/editor/OutputView.ts
@@ -8,25 +8,28 @@ export class OutputView extends Editor {
   extends = [jsx, readOnly];
 
   main = get(Main);
+
+  build(source: string){
+    const { document, options } = this.main;
+
+    try {
+      this.text = transform(source, options);
+    }
+    catch(error){
+      console.error(error);
+      document.error = "Error while compiling module.";
+    }
+    finally {
+      document.stale = false;
+      // localStorage.setItem("REPL:file", source);
+    }
+  }
   
   ready(){
     const main = this.main;
     const doc = main.document;
 
-    const release = doc.get(({ source }) => {
-      try {
-        this.text = transform(source, main.options);
-      }
-      catch(error){
-        console.error(error);
-        doc.error = "Error while compiling module.";
-      }
-      finally {
-        doc.stale = false;
-        // localStorage.setItem("REPL:file", source);
-      }
-    });
-
+    const release = doc.get(x => this.build(x.source));
     const release2 = main.get("fontSize", () => {
       this.view.requestMeasure();
     });
