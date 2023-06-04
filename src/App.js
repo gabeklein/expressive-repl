dbc47fd6e4aece3ebc0e963c59f8c29b086ffb7d
Gabe Klein <gabe@termtm.com>
2023-06-04 00:14:31 -0400

    collapse container element

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import { Footer, Header } from './components/common';
 import { Interface, REPL } from './editor';
 
 export const App = () => {
-  window: {
-    height: vh(100);
-    boxSizing: border-box;
-    padding: 0, 10;
-    gridRows: min, minmax(0, "1fr"), min;
-    overflow: hidden;
-  }
+  height: vh(100);
+  boxSizing: border-box;
+  padding: 0, 10;
+  gridRows: min, minmax(0, "1fr"), min;
+  overflow: hidden;
 
   <Provider for={REPL}>
-    <window>
-      <Header />
-      <Interface />
-      <Footer />
-    </window>
+    <Header />
+    <Interface />
+    <Footer />
   </Provider>
 }
 
