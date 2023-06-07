f2f93735c385d1e5b16be70ca875db03fa70d1a5
Gabe Klein <gabe@termtm.com>
2023-06-07 00:24:30 -0400

    collapse createHandle into component

--- src/layout/Control.ts
+++ src/layout/Control.ts
@@ -25,6 +25,7 @@ export class Control extends Model {
   output = get(() => this.getOutput);
 
   container = ref(this.applyLayout);
+
   separator = set(() => this.parent?.separator || "div") as any;
 
   children = set([], value => {
@@ -54,46 +55,21 @@ export class Control extends Model {
     const output: ReactNode[] = [];
 
     items.forEach((child: any, i, array) => {
-      const key = i * 2;
+      let index = i * 2;
 
       output.push(
-        React.cloneElement(child, {
-          index: key, key, ...child.props
-        })
+        React.cloneElement(child, { ...child.props, key: index, index })
       );
 
-      if(i + 1 < array.length)
-        output.push(this.createHandle(key + 1));
+      if(i + 1 < array.length){
+        index++;
+        output.push(
+          React.createElement(Separate, { key: index, index })
+        );
+      }
     });
 
     return output;
-  }
-
-  protected createHandle(key: number){
-    const { items, parent, separator } = this;
-    const events = this.watch(key);
-    const ref = createRef(events);
-
-    let pull: ((value: any) => void) | undefined;
-    let push: ((value: any) => void) | undefined;
-
-    if(parent){
-      const key = this.index!;
-
-      if(key > 1)
-        pull = createRef(events, parent.watch(key - 1));
-
-      if(key < items.length - 2)
-        push = createRef(events, parent.watch(key + 1));
-    }
-
-    return React.createElement(separator, {
-      key,
-      index: key,
-      pull,
-      push,
-      ref
-    })
   }
 
   public watch(index: number){
@@ -123,6 +99,29 @@ export class Control extends Model {
       }
     }
   }
+}
+
+const Separate: React.FC<{ index: number }> = (props) => {
+  return Control.get($ => {
+    const { separator, parent, items } = $;
+    const events = $.watch(props.index);
+    const ref = createRef(events);
+
+    let pull: ((value: any) => void) | undefined;
+    let push: ((value: any) => void) | undefined;
+
+    if(parent){
+      const key = $.index!;
+
+      if(key > 1)
+        pull = createRef(events, parent.watch(key - 1));
+
+      if(key < items.length - 2)
+        push = createRef(events, parent.watch(key + 1));
+    }
+
+    return React.createElement(separator, { pull, push, ref });
+  });
 }
 
 function flattenChildren(input: ReactNode): ReactNode[] {
