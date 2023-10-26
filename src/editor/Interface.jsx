ececf0671852970670625274324ba83ed6166be7
Gabe Klein <gabe@termtm.com>
2023-10-25 23:37:39 -0400

    improved editor components

--- src/editor/Interface.jsx
+++ src/editor/Interface.jsx
@@ -1,4 +1,3 @@
-import { Editor } from 'codemirror/Editor';
 import { Col, Row } from 'layout';
 import Preview from 'preview/Preview';
 import { Fragment } from 'react';
@@ -11,40 +10,35 @@ export const Interface = () => {
   const { layout } = Main.get();
 
   <Row>
-    <View type={InputEditor} />
+    <InputJSX />
     {layout == "compact" ? (
       <Col>
-        <MockOutput />
+        <OutputJS />
         <Preview />
       </Col>
     ) : layout == "fill" ? (
       <Fragment>
-        <MockOutput />
+        <OutputJS />
         <Preview />
       </Fragment>
     ) : layout == "view" ? (
       <Preview />
     ) : (
-      layout == "code" && <MockOutput />
+      layout == "code" && <OutputJS />
     )}
   </Row>
 }
 
 /** @type {React.FC} */
-const MockOutput = () => {
-  <View type={OutputView} />
+const OutputJS = () => {
+  const { element } = OutputView.use();
+
+  <this ref={element} />
 }
 
-/**
- * @type {React.FC<{
- *   type: typeof Editor;
- *   className?: string;
- * }>}
- **/
-export const View = ({ type, className }) => {
-  const { element } = type.use();
+/** @type {React.FC} */
+const InputJSX = () => {
+  const { element } = InputEditor.use();
 
-  overflow: hidden;
-
-  <this ref={element} className={className} />
+  <this ref={element} />
 }
