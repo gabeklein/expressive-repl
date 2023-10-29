898f8b35fdfa257fc620f0983633b8c94e56af4f
Gabe Klein <gabe@termtm.com>
2023-10-29 11:18:10 -0400

    remove unused method in OutputView

--- src/editor/OutputView.ts
+++ src/editor/OutputView.ts
@@ -2,31 +2,15 @@ import { get } from '@expressive/react';
 import { Editor, jsx, readOnly } from 'codemirror/Editor';
 
 import { Document } from './Document';
-import { Main } from './Main';
-import { transform } from './transform';
 
 export class OutputView extends Editor {
-  main = get(Main);
   doc = get(Document);
 
   extends(){
-    return [jsx, readOnly];
-  }
-
-  build(source: string){
-    const { document, options } = this.main;
-
-    try {
-      this.text = transform(source, options);
-    }
-    catch(error){
-      console.error(error);
-      document.error = "Error while compiling module.";
-    }
-    finally {
-      document.stale = false;
-      // localStorage.setItem("REPL:file", source);
-    }
+    return [
+      jsx,
+      readOnly
+    ];
   }
   
   onReady(){
