fec72d5abc194884684b14f7f3d505483fcfcc4e
Gabe Klein <gabe@termtm.com>
2023-11-09 15:05:05 -0500

    improved prettify step

--- src/editor/transform.ts
+++ src/editor/transform.ts
@@ -62,67 +62,61 @@ export function transform(input: string){
   };
 }
 
+export function prettify(code: string){
+  try {
+    code = Prettier.format(code, {
+      // fake parser! returns AST we have
+      // parser: () => output.ast,
+      parser: "babel",
+      plugins: [ parserBabel ],
+      singleQuote: false, 
+      trailingComma: "none", 
+      jsxBracketSameLine: true,
+      tabWidth: 2,
+      printWidth: 60
+    });
+
+    return Object
+      .values(transforms)
+      .reduce((x, fx) => fx(x), code);
+  }
+  catch(err){
+    throw new Error("Failed to prettify source.");
+  }
+}
+
+const transforms: Record<string, (x: string) => string> = {
+  statementLineSpacing: (x: string) =>
+    x.replace(/^(.+?)\n(export|const|let)/gm, "$1\n\n$2"),
+
+  jsxReturnSpacing: (x: string) =>
+    x.replace(/^(.+?[^{])\n(\s+return (?=\(|<))/gm, "$1\n\n$2"),
+
+  removeDoubleLines: (x: string) =>
+    x.replace(/\n{3,}/g, "\n\n"),
+
+  spaceOutBlocks: (x: string) =>
+    x.replace(/([\t \r]*\n)([\)\}\]]+;?)([\t \r]*\n{1})(\s*[^\ni])/g, "$1$2$3\n$4"),
+
+  spaceAfterImports: (x: string) =>
+    x.replace(/(from ".+";?)([\t \r]*\n)([^\ni])/g, "$1$2\n$3"),
+
+  tabCharactersMustDie: (x: string) =>
+    x.replace(/\t/g, "  "),
+
+  compactStylesInclude: (x: string) =>
+    x.replace(/Styles\.include\(\n\s+`\n([^`]+)[^;]+;/g, "Styles.include(`\n$1`);"),
+
+  ensureSpaceBeforeCSS: (x: string) =>
+    x.replace(/(\n[\S\t ]+\n)(CSS\.put)/, "$1\n$2"),
+
+  removeTrailingline: (x: string) =>
+    x.replace(/\n$/, "")
+};
+
 export function hash(from: string){
   let hash = 0;
   for(let i = 0; i < from.length; i++)
     hash = ((hash << 5) - hash) + from.charCodeAt(i);
   return hash;
 }
-
-export function prettify(code: string){
-  code = Prettier.format(code, {
-    // fake parser! returns AST we have
-    // parser: () => output.ast,
-    parser: "babel",
-    plugins: [ parserBabel ],
-    singleQuote: false, 
-    trailingComma: "none", 
-    jsxBracketSameLine: true,
-    tabWidth: 2,
-    printWidth: 60
-  });
-
-  for(const fix of transforms)
-    code = fix(code);
-
-  return code;
-}
-
-const statementLineSpacing = (x: string) =>
-  x.replace(/^(.+?)\n(export|const|let)/gm, "$1\n\n$2")
-
-const jsxReturnSpacing = (x: string) =>
-  x.replace(/^(.+?[^{])\n(\s+return (?=\(|<))/gm, "$1\n\n$2")
-
-const removeDoubleLines = (x: string) =>
-  x.replace(/\n{3,}/g, "\n\n")
-
-const spaceOutBlocks = (x: string) =>
-  x.replace(/([\t \r]*\n)([\)\}\]]+;?)([\t \r]*\n{1})(\s*[^\ni])/g, "$1$2$3\n$4")
-
-const spaceAfterImports = (x: string) =>
-  x.replace(/(from ".+";?)([\t \r]*\n)([^\ni])/g, "$1$2\n$3")
-
-const tabCharactersMustDie = (x: string) =>
-  x.replace(/\t/g, "  ")
-
-const compactStylesInclude = (x: string) =>
-  x.replace(/Styles\.include\(\n\s+`\n([^`]+)[^;]+;/g, "Styles.include(`\n$1`);")
-
-const ensureSpaceBeforeCSS = (x: string) =>
-  x.replace(/(\n[\S\t ]+\n)(CSS\.put)/, "$1\n$2")
-
-const removeTrailingline = (x: string) =>
-  x.replace(/\n$/, "")
-
-const transforms = [
-  statementLineSpacing,
-  jsxReturnSpacing,
-  spaceOutBlocks,
-  spaceAfterImports,
-  removeDoubleLines,
-  tabCharactersMustDie,
-  compactStylesInclude,
-  ensureSpaceBeforeCSS,
-  removeTrailingline
-];
