3c00903b40976bc3947c71a6fe2d01cab03bdb46
Gabe Klein <gabe@termtm.com>
2025-02-26 14:12:27 -0500

    add vite-plugin-monaco

--- packages/repl/vite.config.ts
+++ packages/repl/vite.config.ts
@@ -1,12 +1,14 @@
 import jsx from '@expressive/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import monaco from 'vite-plugin-monaco-editor';
 import paths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     jsx(),
     react(),
-    paths()
+    paths(),
+    monaco({})
   ]
 })
